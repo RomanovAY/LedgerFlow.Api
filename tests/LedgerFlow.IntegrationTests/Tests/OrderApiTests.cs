80b0259a259c8b9a09e66a76ca28d9b7b7cb108c
RomanovAY <romanovonline@mail.ru>
2026-07-31 06:10:34 +0300

    Fix

--- tests/LedgerFlow.IntegrationTests/Tests/OrderApiTests.cs
+++ tests/LedgerFlow.IntegrationTests/Tests/OrderApiTests.cs
@@ -22,7 +22,6 @@ public class OrderApiTests : IClassFixture<CustomWebApplicationFactory>
 		_factory.OutputHelper = outputHelper;
 
 		_client = _factory.CreateClient();
-		_client.BaseAddress = new Uri("https://localhost:7292");
 		_faker = new Faker("ru");
 	}
 
